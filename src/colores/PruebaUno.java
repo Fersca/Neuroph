a2f7773a1f71a570b0be79c9f938ff575e80685a
Fernando Scasserra <fersca@hotmail.com>
2014-04-01 18:24:38 -0300

    comentarios

--- src/colores/PruebaUno.java
+++ src/colores/PruebaUno.java
@@ -18,86 +18,100 @@ public class PruebaUno {
 
 	public static void main(String[] args) throws Exception {
 
+		//Obtiene la foto
 		String foto = "http://cdn.inaxiom.net/web/wp-content/uploads/2009/03/renault-sandero-pack-00.jpg";
 		
+		//detecta los colores de la foto
 		PruebaUno p1 = new PruebaUno();
-		String colorAuto = p1.detectColors(foto,true);
+		String colorAuto = p1.detectColors(foto);
 		
-	    File file1 = new File("/home/fersca/coloresUnAuto.txt");
+		//Crea el file con el resultado de los colores de la foto
+	    File file1 = new File("coloresUnAuto.txt");
 	    Writer output = new BufferedWriter(new FileWriter(file1));
 		output.write(colorAuto+"0,0,0,0,0,0,0");
 		output.close();	    
 
-		DataSet trainingSet = DataSet.createFromFile("/home/fersca/coloresUnAuto.txt", 300, 7, ",");
+		//Crea un trainig set en base a este archivo
+		DataSet trainingSet = DataSet.createFromFile("coloresUnAuto.txt", 300, 7, ",");
 		
-        //NeuralNetwork loadedMlPerceptron = NeuralNetwork.load("myMlPerceptron.nnet");
-		//NeuralNetwork loadedMlPerceptron = NeuralNetwork.load("myMlPerceptronCentro.nnet");
-		//NeuralNetwork loadedMlPerceptron = NeuralNetwork.load("myMlPerceptronCentro50.nnet");
+		//Carga la red (ya previamente entrenada)
 		NeuralNetwork loadedMlPerceptron = NeuralNetwork.load("myMlPerceptronCentro50-plata.nnet");
 		
+		//Por cada uno de los trainig sets, calcula el resultado
         for(DataSetRow testSetRow : trainingSet.getRows()) {
+        	
+        	//setea como input el set que estamos procesando
         	loadedMlPerceptron.setInput(testSetRow.getInput());
+        	
+        	//calcula el resultado de la red
         	loadedMlPerceptron.calculate();
+        	
+        	//obtiene el output de la red
             double[] networkOutput = loadedMlPerceptron.getOutput();	
 
-            System.out.println("Input: " + Arrays.toString( testSetRow.getInput() ) );
-            System.out.println(" Output: " + Arrays.toString( networkOutput) );
-            
-            int posMax=0;
-            int posMax2=0;
-            int pos=0;
-            int pos2=0;
-            double max=0;
-            double max2=0;
-            
-            for (double numero : networkOutput) {
-            	System.out.println("cant: "+numero);
-            	if (numero>max){
-            		max=numero;
-            		posMax=pos;
-            	}
-    			pos++;
-    		}
-
-            networkOutput[posMax]=0;
-            
-            for (double numero : networkOutput) {
-            	if (numero>max2){
-            		max2=numero;
-            		posMax2=pos2;
-            	}
-    			pos2++;
-    		}
-
-            
-            String color="";
-            String color2="";
-            if (posMax==0) color="Plata";
-            if (posMax==1) color="Rojo";
-            if (posMax==2) color="Gris";
-            if (posMax==3) color="Negro";
-            if (posMax==4) color="Blanco";
-            if (posMax==5) color="Azul";
-            if (posMax==6) color="Verde";
-            if (posMax2==0) color2="Plata";
-            if (posMax2==1) color2="Rojo";
-            if (posMax2==2) color2="Gris";
-            if (posMax2==3) color2="Negro";
-            if (posMax2==4) color2="Blanco";
-            if (posMax2==5) color2="Azul";
-            if (posMax2==6) color2="Verde";
-            
-            System.out.println("Color: " + color+" - "+(int)(max*100)+"%" );
-            System.out.println("Color: " + color2+" - "+(int)(max2*100)+"%" );
+            //procesa el output
+            processOutput(testSetRow, networkOutput);
             
         }
 		
         
 	}
 
+	private static void processOutput(DataSetRow testSetRow, double[] networkOutput) {
+		System.out.println("Input: " + Arrays.toString( testSetRow.getInput() ) );
+		System.out.println(" Output: " + Arrays.toString( networkOutput) );
+		
+		int posMax=0;
+		int posMax2=0;
+		int pos=0;
+		int pos2=0;
+		double max=0;
+		double max2=0;
+		
+		for (double numero : networkOutput) {
+			System.out.println("cant: "+numero);
+			if (numero>max){
+				max=numero;
+				posMax=pos;
+			}
+			pos++;
+		}
+
+		networkOutput[posMax]=0;
+		
+		for (double numero : networkOutput) {
+			if (numero>max2){
+				max2=numero;
+				posMax2=pos2;
+			}
+			pos2++;
+		}
+
+		
+		String color="";
+		String color2="";
+		if (posMax==0) color="Plata";
+		if (posMax==1) color="Rojo";
+		if (posMax==2) color="Gris";
+		if (posMax==3) color="Negro";
+		if (posMax==4) color="Blanco";
+		if (posMax==5) color="Azul";
+		if (posMax==6) color="Verde";
+		if (posMax2==0) color2="Plata";
+		if (posMax2==1) color2="Rojo";
+		if (posMax2==2) color2="Gris";
+		if (posMax2==3) color2="Negro";
+		if (posMax2==4) color2="Blanco";
+		if (posMax2==5) color2="Azul";
+		if (posMax2==6) color2="Verde";
+		
+		System.out.println("Color: " + color+" - "+(int)(max*100)+"%" );
+		System.out.println("Color: " + color2+" - "+(int)(max2*100)+"%" );
+	}
+
 	private static final int TAMANO_BLOQUE = 20;
 	
-	public String detectColors(String foto, Boolean isURL) throws Exception {
+	public String detectColors(String foto) throws Exception {
 					
 		int c;
 		int red=0;
@@ -112,7 +126,7 @@ public class PruebaUno {
 
 			//Ontiene la imagen a calcular
 			BufferedImage image;
-			if (isURL){
+			if (foto.contains("http://")||foto.contains("https://")){
 				URL url = new URL(foto);
 				image = ImageIO.read(url);
 			}
